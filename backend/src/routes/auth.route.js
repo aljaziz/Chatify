04b68361532fee80f10a745c8aec9723bdf66966
aljaziz <turjatheharbingerofdeath@gmail.com>
2025-09-25 18:36:17 +0600

    login and logout functionality added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", signup);
 
-authRouter.get("/login", (req, res) => {
-    res.send("Login endpoint");
-});
+authRouter.post("/login", login);
 
-authRouter.get("/logout", (req, res) => {
-    res.send("Logout endpoint");
-});
+authRouter.post("/logout", logout);
 
 export default authRouter;
